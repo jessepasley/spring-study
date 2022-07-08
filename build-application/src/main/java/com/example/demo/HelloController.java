451a67c71e628c45dcf9da0395e40b43bf8bb2f4
arjunmetre <arjunprojectexecute@gmail.com>
2022-07-09 01:55:27 +0800

    #1

--- build-application/src/main/java/com/example/demo/HelloController.java
+++ build-application/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+*@author : Arjun Metre
+*/
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@PostMapping("/post")
+	public String getLearnerDetails() {
+
+		return "Hello From post";
+	}
+
+	@PutMapping("/update")
+	public String putMethod() {
+		return "";
+	}
+
+	@DeleteMapping("/delete")
+	public String deleteMethod() {
+		return "";
 	}
 
 }
